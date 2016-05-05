9b90f754330da21c48f8d259e8b44389ced49ec4
Sushanth Reddy <sushanth53@gmail.com>
2016-05-05 14:40:08 -0400

    Update models.py
    Current django latest below module chaged from
    from django.contrib.contenttypes import generic
    with
    from django.contrib.contenttypes.fields import GenericForeignKey

--- djangoratings/models.py
+++ djangoratings/models.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
-from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.auth.models import User
 
 try:
